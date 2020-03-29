0c3afcde9225ce50ee8a4f77dea9bf32f46e0a1f
Your Name <you@example.com>
2020-03-29 10:13:27 +0900

    Task Server実装, DB実装 1

---
+++ server/src/tasks/dto/get-tasks-filter.dto.ts
@@ -0,0 +1,15 @@
+import { TaskStatus } from '../task.model';
+import { IsOptional, IsIn } from 'class-validator';
+
+export class GetTasksFilterDto {
+    @IsOptional()
+    @IsIn([
+        TaskStatus.OPEN,
+        TaskStatus.IN_PROGRESS,
+        TaskStatus.DONE
+    ])
+    status: TaskStatus;
+
+    @IsOptional()
+    search: string;
+}
